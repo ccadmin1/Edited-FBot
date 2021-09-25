4667fbcc90fee21c678a2e1ef91eea30e7aa3cc2
WARRIORS <56763972+ccadmin1@users.noreply.github.com>
2021-09-26 00:03:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1,4 +1,3 @@
-Kanged From @TroJanZheX
 from info import AUTH_CHANNEL, AUTH_USERS, CUSTOM_FILE_CAPTION, API_KEY, AUTH_GROUPS
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram import Client, filters
